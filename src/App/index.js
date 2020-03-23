02929b4322d97d1f399de5f26622fe46b9e7bc30
Gavin <gavin@bwsonline.co.za>
2020-03-23 14:07:37 +0200

    Create context & useState()

--- src/App/index.js
+++ src/App/index.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Context from '@/contexts/App';
 
 const App = () => {
+	const [count, setCount] = useState(0);
+	const [count2, setCount2] = useState(0);
+
 	return (
 		<>
-			<h1>Hello world</h1>
+			<button onClick={() => setCount(count + 1)}>Count {count}</button>
+			<button onClick={() => setCount2(count2 + 1)}>Count2 {count2}</button>
+
+			<Context.Provider value={[count, setCount]}>
+				<h1>Hello world</h1>
+			</Context.Provider>
 		</>
 	);
 };

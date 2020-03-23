0939084a3c258846271a7d4429bd3571f9a9c5bc
Gavin <gavin@bwsonline.co.za>
2020-03-23 14:50:23 +0200

    Use effect hook

--- src/App/HomePage/index.js
+++ src/App/HomePage/index.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useAppContext } from '@/contexts/App';
 import HomePageContext from '@/contexts/HomePage';
 import List from './List';
 
 const HomePage = () => {
 	const { count: [count, setCount], count2: [count2, setCount2] } = useAppContext();
+
+	const [list, setList] = useState([]);
+
+	useEffect(() => {
+		console.log('Çomponent rerendered');
+	});
+
+	useEffect(() => {
+		console.log('Çomponent did mount');
+	}, []);
+
+	useEffect(() => {
+		console.log('Runs only if count changes or comp did mount');
+	}, [count]);
+
+	useEffect(() => {
+		setTimeout(() => {
+			setList([{ label: 'John doe' }, { label: 'Jane doe ' }]);
+		}, 5000);
+	}, []);
 
 	return (
 		<>
@@ -16,7 +36,7 @@ const HomePage = () => {
 				Count2 from Home {count2}
 			</button>
 
-			<HomePageContext.Provider value={[{ label: 'John doe' }, { label: 'Jane doe' }]}>
+			<HomePageContext.Provider value={list}>
 				<List />
 			</HomePageContext.Provider>
 		</>
